c6f1cbdaeb5176774f1d1d57fdb86858afe3db19
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-04-19 10:41:14 +0200

    inc version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="gridtools",
-    version="0.0.1",
+    version="0.1.0",
     license='GPL 3',
     author='Norman Fomferra',
     maintainer='Brockmann Consult GmbH',
