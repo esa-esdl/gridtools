27329b6380710cd683ff5ca287f5f307c695d441
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-04-22 20:12:57 +0200

    added some tests for checking numba behaviour

--- test/test_numba.py
+++ test/test_numba.py
@@ -4,17 +4,28 @@ import numba as nb
 import numpy as np
 
 
-@nb.jit
+@nb.jit(nopython=True)
 def return_a_tuple(n):
-    out = np.arange(n, dtype=np.float64)
-    return out, n, n+4, 'A'
+    out = np.array([n], dtype=np.float64)
+    return out, n, n + 4
+
+
+@nb.jit(nopython=False)
+def recursive_sum(a):
+    if a.size == 1:
+        return a[0]
+    else:
+        return a[0] + recursive_sum(a[1:])
 
 
 class NumbaTest(unittest.TestCase):
     def test_can_return_a_tuple(self):
-        out, n, m, s = return_a_tuple(8)
-        self.assertEqual((8,), out.shape)
+        out, n, m = return_a_tuple(8)
+        self.assertEqual((1,), out.shape)
         self.assertEqual(np.float64, out.dtype)
         self.assertEqual(8, n)
         self.assertEqual(8 + 4, m)
-        self.assertEqual('A', s)
+
+    def test_can_do_recursion(self):
+        out = recursive_sum(np.array([1., 2, 3]))
+        self.assertEqual(out, 6)
