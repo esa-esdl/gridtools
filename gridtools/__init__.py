7918fc7d3a8ff4f17c0b994475cd808309e5b314
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-07-12 14:49:00 +0200

    added version.py and now use it

--- gridtools/__init__.py
+++ gridtools/__init__.py
@@ -1,1 +1,1 @@
-__version__ = '0.2.0'
+from .version import version as __version__
--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 from setuptools import setup
 
+
+def get_version():
+    version_file = 'gridtools/version.py'
+    locals = {}
+    try:
+        execfile(version_file, None, locals)
+    except NameError:
+        with open(version_file) as fp:
+            exec(fp.read(), None, locals)
+    return locals.get('version', '0.0.0')
+
+
+# Same effect as "from ect import __version__", but avoids importing ect:
+__version__ = get_version()
+
 setup(
     name="gridtools",
-    version="0.2.0",
+    version=__version__,
     license='GPL 3',
     author='Norman Fomferra',
     maintainer='Brockmann Consult GmbH',
